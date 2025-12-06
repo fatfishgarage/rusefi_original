9909d23bbedef6876d038b4f14c43a108e8a89d5
fatfishgarage <fatfish.garage@gmail.com>
2025-12-06 23:48:07 +0100

    Update trigger_suzuki.cpp

--- firmware/controllers/trigger/decoders/trigger_suzuki.cpp
+++ firmware/controllers/trigger/decoders/trigger_suzuki.cpp
@@ -10,28 +10,24 @@
 #include "trigger_suzuki.h"
 
 void initializeSuzukiG13B(TriggerWaveform *s) {
-	s->initialize(FOUR_STROKE_CAM_SENSOR, SyncEdge::RiseOnly);
+	s->initialize(FOUR_STROKE_CRANK_SENSOR, SyncEdge::RiseOnly);
 
-	float w = 5;
-	float specialTooth = 20;
+	int w = 5;
+	
+	s->addToothRiseFall(95, w);
 
-	s->addEvent720(180 - w, TriggerValue::RISE);
-	s->addEvent720(180, TriggerValue::FALL);
+	s->addToothRiseFall(125, w);
+	
+	s->addToothRiseFall(180, w);
 
-	s->addEvent720(2 * specialTooth + 180 - w, TriggerValue::RISE);
-	s->addEvent720(2 * specialTooth + 180, TriggerValue::FALL);
+	s->addToothRiseFall(275, w);
 
-	s->addEvent720(360 - w, TriggerValue::RISE);
-	s->addEvent720(360, TriggerValue::FALL);
+	s->addToothRiseFall(305, w);
 
-	s->addEvent720(540 - w, TriggerValue::RISE);
-	s->addEvent720(540, TriggerValue::FALL);
+	s->addToothRiseFall(360, w);
 
-	s->addEvent720(720 - w, TriggerValue::RISE);
-	s->addEvent720(720, TriggerValue::FALL);
-
-	s->setTriggerSynchronizationGap(0.22);
-	s->setSecondTriggerSynchronizationGap(1);
+	s->isSynchronizationNeeded = false;
+	s->shapeWithoutTdc = true;
 }
 
 void initializeSuzukiK6A(TriggerWaveform *s) {
